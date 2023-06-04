abc9293c6eb7115f602eac83b31d6d6ad242d5e3
GuedsonRibeiro <guedson.ge@gmail.com>
2023-06-04 18:02:38 -0300

    8

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-// Código JavaScript para a comunicação com o backend
+  // Código JavaScript para a comunicação com o backend
 document.addEventListener('DOMContentLoaded', function() {
-    // Obtenha uma referência ao formulário de doação
-    const donationForm = document.querySelector('form');
+    // Obtenha uma referência à tabela de doações
+    const donationTable = document.getElementById('donationTable');
   
-    // Adicione um listener de evento para o envio do formulário
+    // Função para buscar os dados do banco de dados e preencher a tabela
+    function fetchDonations() {
+      fetch('http://107.23.247.169/api/doacao') // substitua pelo endpoint correto
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          // Limpar o conteúdo atual da tabela
+          donationTable.innerHTML = '';
+  
+          // Preencher a tabela com os dados retornados
+          data.forEach(function(donation) {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+              <td>${donation.name}</td>
+              <td>${donation.email}</td>
+              <td>${donation.amount}</td>
+            `;
+            donationTable.appendChild(row);
+          });
+        })
+        .catch(function(error) {
+          console.log(error);
+          // Trate os erros de conexão ou outros erros do lado do cliente
+        });
+    }
+  
+    // Chamada inicial para buscar e exibir as doações
+    fetchDonations();
+  
+    // Adicione um listener de evento para o envio do formulário de doação
+    const donationForm = document.getElementById('donationForm');
     donationForm.addEventListener('submit', function(event) {
       event.preventDefault(); // Evita que a página seja recarregada ao enviar o formulário
   
@@ -21,21 +52,22 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         body: JSON.stringify({ name: name, email: email, amount: amount })
       })
-      .then(function(response) {
-        // Verifique a resposta do backend e tome as ações apropriadas
-        if (response.ok) {
-          // A doação foi bem-sucedida
-          alert('Obrigado pela sua doação!');
-          donationForm.reset(); // Limpa os campos do formulário
-        } else {
-          // A doação falhou
-          alert('Ocorreu um erro ao processar a sua doação. Por favor, tente novamente mais tarde.');
-        }
-      })
-      .catch(function(error) {
-        console.log(error);
-        // Trate os erros de conexão ou outros erros do lado do cliente
-        alert('Ocorreu um erro ao processar a sua doação. Por favor, tente novamente mais tarde.');
-      });
-    });
-  });
+        .then(function(response) {
+          // Verifique a resposta do backend e tome as ações apropriadas
+          if (response.ok) {
+            // A doação foi bem-sucedida
+            alert('Obrigado pela sua doação!');
+            donationForm.reset(); // Limpa os campos do formulário
+            fetchDonations(); // Atualiza a tabela com as doações mais recentes
+          } else {
+// A doação falhou
+alert('Ocorreu um erro ao processar a sua doação. Por favor, tente novamente mais tarde.');
+}
+})
+.catch(function(error) {
+console.log(error);
+// Trate os erros de conexão ou outros erros do lado do cliente
+alert('Ocorreu um erro ao processar a sua doação. Por favor, tente novamente mais tarde.');
+});
+});
+});
